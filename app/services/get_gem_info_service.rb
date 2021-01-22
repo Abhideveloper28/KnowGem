d29a1d1204052722749737f14854e588a64b57ca
Abhi <abhi.dev@gmail.com>
2021-01-22 18:36:20 +0530

    Optimise gem_info_service

--- app/services/get_gem_info_service.rb
+++ app/services/get_gem_info_service.rb
@@ -4,8 +4,27 @@ require 'open-uri'
 class GetGemInfoService
   GEM_URL = "https://rubygems.org/gems"
 
-  def gem_scraping(gem_name)
-    doc = Nokogiri::HTML(URI.open("#{GEM_URL}/#{gem_name}"))
+  def initialize(gem_name)
+    @gem_name = gem_name
+  end
+
+  def gem_description
+    gem_info = GemInfo.find_by(name: @gem_name)
+    if gem_info.present? && gem_info.description.present?
+      puts 'Getting info from database'
+      gem_info.description
+    else
+      puts 'Getting info from scraping'
+      description = gem_scraping
+      StoreGemInfoJob.new(@gem_name, description).perform_now
+      description
+    end
+  end
+
+  private
+
+  def gem_scraping
+    doc = Nokogiri::HTML(URI.open("#{GEM_URL}/#{@gem_name}"))
 
     gem_desc = doc.css('div.gem__desc p').text
   end
